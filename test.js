43c99fd7de82f39c2fb628e101a8597c4fbde630
Arthur Opirata <arthurcarneiro098@gmail.com>
2024-04-05 14:17:19 -0300

    sdfs

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
 //lampada
+class Lampada{
+    constructor(nome){
+        this.nome = nome
+        this.acesa= false
+    }
+    ligar(){
+        this.acesa = true
+    }
+    desligar(){
+        this.acesa = false
+    }
+    static troca(){
+        console.log("trocando bocal")
+    }
+
+
+}
+const lam = new Lampada('LED')
+lam.ligar()
+lam.desligar()
+console.log(lam)
+Lampada.troca()
